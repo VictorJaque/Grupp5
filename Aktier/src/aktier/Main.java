41cb1c6e67c7bbc0b37b575e44fe8c4be468e2af
Kalle Nilsson <domare1@hotmail.com>
2021-03-02 11:49:56 +0100

    Test av git

--- Aktier/src/aktier/Main.java
+++ Aktier/src/aktier/Main.java
@@ -17,6 +17,8 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Test git");
+        System.out.println("Test git");
+        System.out.println("Test git");
     }
     
 }
