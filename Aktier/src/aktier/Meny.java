245ce00f929d580c30c1d75024fa697855936f91
Kalle Nilsson <domare2@hotmail.com>
2021-03-02 15:26:51 +0100

    Fortsatt utveckling av menyvalet. GIT test...

--- Aktier/src/aktier/Meny.java
+++ Aktier/src/aktier/Meny.java
@@ -99,7 +99,8 @@ public class Meny {
                         break;
                         default:
                             System.out.println("Du skrev inte in något av alternativen");
-                            System.out.println("x");
+                            System.out.println("git test");
+                            System.out.println("git test");
                             AktieMarknadenMainMenu();
                             
                         
