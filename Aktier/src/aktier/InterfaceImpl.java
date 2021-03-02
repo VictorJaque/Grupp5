e7b42a4b02809fc0f70925c025f1620f80cd1898
Preecha Häll <preecha.hall@gmail.com>
2021-03-02 15:36:18 +0100

    lägger till implementering av interface

---
+++ Aktier/src/aktier/InterfaceImpl.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aktier;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author preec
+ */
+public class InterfaceImpl implements UserInterface {
+    
+    
+    private static ArrayList<User> list = new ArrayList<User>();
+
+    @Override
+    public boolean isLogin(String username, String password) {
+        //antar att logningen fail
+        boolean flag = false; 
+        
+        for(User u: list){
+            if(u.getName().equals(username) && u.getPassword().equals(password)) {
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    @Override
+    public void register(User user) {
+         //Registerings logic
+         //Spara alla användare in i en samling
+         //
+         list.add(user);
+    }
+    
+}
