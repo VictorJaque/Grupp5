93579f8967bdf5f9a0fd01eeca1a2d5bb80c1421
Kalle Nilsson <domare2@hotmail.com>
2021-03-03 03:40:31 +0100

    Fullständigt menyval för uttag + kontosaldo är nu inlagt. Flyttat ut instansen av Aktiemarknaden från metoden den befann sig i. Den ligger nu i klassen för att göra som static för att återanvändas utan initiering av ett nytt i alla menymetoder.

--- Aktier/src/aktier/Meny.java
+++ Aktier/src/aktier/Meny.java
@@ -5,6 +5,7 @@
  */
 package aktier;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -12,6 +13,9 @@ import java.util.Scanner;
  * @author Kalle
  */
 public class Meny {
+    public static float userValueInput;
+    public static  String choiceMainMenu;
+    public static Aktiemarknad userMoneyAndCurrency = new Aktiemarknad(userValueInput, choiceMainMenu);
     //userBalance används för att lagra det befintliga värdet av value för att sedan kunna öka eller minska den beroende på insättning/uttag
     public static float userBalance = 0;
     //static scanner för att nå den i hela klassen
@@ -19,8 +23,11 @@ public class Meny {
     
     //Menyn som visas först vid exekvering
     public static void MenuStart(){
-        System.out.println("Registrera dig");
-        System.out.println("Logga in");
+        System.out.println("1. Registrera dig");
+        System.out.println("2. Logga in");
+        String choiceMenuStart = scan.next();
+        
+        
         
         //Inväntar en metod från registreringsklassen som ska initieras nedan
         
@@ -33,10 +40,10 @@ public class Meny {
         
         
         
-        String choice = scan.next();
+        String choiceMenuAfterLogin = scan.next();
         
         //lägg in en metod från bankklass eller likande här
-        switch (choice){
+        switch (choiceMenuAfterLogin){
             case "1": 
                 BankMainMenu();                
                 break;
@@ -79,46 +86,60 @@ public class Meny {
                     
                     
             }
+            
+            //Nedan är kod som berör Aktiemarknaden
             }
             public static void AktieMarknadenMainMenu(){
-                System.out.println("1. Ta ut pengar");
+                System.out.println("1. Sätt in pengar");
+                System.out.println("2. Ta ut pengar");
                 System.out.println("3. Kolla saldo");
-                System.out.println("3. Sätt in pengar");
+                
                 
                 
                 String choiceMainMenu = scan.next();
                 
                 switch (choiceMainMenu){
+                    
                     case "1":
-                        //här ska en metod från aktiemarknadsklassen initieras
-                        break;
-                    case "2":
-                        //här ska en metod från aktiemarknadsklassen initieras
-                        break;
-                    case "3":
                         System.out.println("Hur mycket vill du sätta in?");
         
-                        float userValueInput = scan.nextInt();
+                        float userValueInput = scan.nextFloat();
                         
+                        
+                        //userBalance är till för att addera det nuvarande värdet med det nya initierade värdet
         
         
                        userBalance = userBalance + userValueInput ;
                         
                         
                         //skapar en instans av klassen 
-                        Aktiemarknad userMoneyAndCurrency = new Aktiemarknad(userValueInput, choiceMainMenu);
+                        
                         
                         
                         //Lägger till de nya insatta värdet på det redan befintliga
                         
                         //genom Klassen aktiemarknad kan vi nå value och currency samt get och set för att sätta och få fram värdet på currency
                         
-                        userMoneyAndCurrency.insertMoney();                                              
+                        userMoneyAndCurrency.insertMoney();
                         
                         break;
-                    case "4":
-                        //här ska en metod från aktiemarknadsklassen initieras
+                    case "2":
+                        System.out.println("Hur mycket vill du ta ut?");
+                        
+                        float userWithdrawalAmount = scan.nextFloat();
+                        
+                        userBalance = userBalance - userWithdrawalAmount;
+                        
+                        userMoneyAndCurrency.withdrawMoney();
+                        
+                        
+                             
                         break;
+                    case "3":
+                        userMoneyAndCurrency.checkBalance();
+                        
+                        break;
+                            
                         default:
                             System.out.println("Du skrev inte in något av alternativen"); 
                             AktieMarknadenMainMenu();
