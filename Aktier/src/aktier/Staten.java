fc8b7f22ad9366a58e6de4166a0c9e43e3278323
Victor Jaque <victor.jaque.mte21lin@tucsweden.se>
2021-03-11 10:10:58 +0100

    La till en klass för staten

---
+++ Aktier/src/aktier/Staten.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aktier;
+
+/**
+ *
+ * @author Victor Jaque <victor.jaque.mte21lin@tucsweden.se>
+ */
+public class Staten {
+    
+}
