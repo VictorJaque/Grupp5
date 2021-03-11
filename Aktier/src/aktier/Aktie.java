a02db4e04ad4e44659f2bfd0466374d59e957e5d
Kalle Nilsson <domare2@hotmail.com>
2021-03-11 01:54:00 +0100

    Aktie klass för initiering av aktier

---
+++ Aktier/src/aktier/Aktie.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aktier;
+
+/**
+ *
+ * @author Kalle
+ */
+public class Aktie {
+    private String aktieID;
+    private String aktieNamn;
+
+    public Aktie(String aktieID ,String aktieNamn) {
+        this.aktieID = aktieID;
+        this.aktieNamn = aktieNamn;
+                
+    }
+    
+    
+
+    public String getAktieNamn() {
+        return aktieNamn;
+    }
+
+    public String getAktieID() {
+        return aktieID;
+    }
+    
+   
+    
+    
+    
+    
+    
+            
+    
+}
