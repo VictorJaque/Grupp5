2521504a47ace5afbc02a0cc44d9effaef4b5e02
Victor Jaque <victor.jaque.mte21lin@tucsweden.se>
2021-03-24 13:46:43 +0100

    Lagt till nurredins kommentarer i koden

--- Aktier/src/aktier/Staten.java
+++ Aktier/src/aktier/Staten.java
@@ -13,12 +13,10 @@ import java.util.concurrent.TimeUnit;
  * @author Nureddin Elmas
  */
 public class Staten extends Money implements MoneyMethods{
-
- 
-
+    //Attribut
     private Scanner scan;
     
-    
+    //Konstruktor
     public Staten(float value, String currency) {
         this.value = value;
         this.currency = currency;
@@ -26,9 +24,11 @@ public class Staten extends Money implements MoneyMethods{
     }
 
  
-
+    //Metoder
+    //Metod för att sätta in pengar
     @Override
     public void insertMoney(float userBalance) throws InterruptedException {
+        //matar in mer pengar än den har
         float amount;
         System.out.println("Sätt in pengar");
         System.out.println("Hur mycket pengar vill du sätta in?");
@@ -37,6 +37,7 @@ public class Staten extends Money implements MoneyMethods{
         System.out.println("Du har nu satt in " + Meny.userBalance + " " + Meny.userInputCurrency + " till ditt konto!");
     }
 
+    //Metod för att ta ut pengar 
     @Override
     public void withdrawMoney(float userBalance) throws InterruptedException {
                 float amount;
@@ -50,31 +51,31 @@ public class Staten extends Money implements MoneyMethods{
             checkBalance(Meny.userBalance);
         }
     }
-    
+    // metod för att kolla saldo 
     @Override
     public void checkBalance(float userBalance) throws InterruptedException {
-        
         System.out.println("Du har " + Meny.userBalance + " " + Meny.userInputCurrency + " på ditt konto" );
         System.out.println("---------------------------------------------------------------");
         TimeUnit.SECONDS.sleep(1);
         System.out.println(" ");
-        
-        
     }
-    
+    // metod för att betala skatt
     public static void BetalaSkatt() throws InterruptedException {
         float komunalskatt = (float) 33.4;
         float userSkatt = 0;
+        // drar pengar baserat på hur mycket pengar du tjänar varje månad 
         System.out.print("Hur mycket tjänar du varje månad? ");
         Scanner newScan = new Scanner(System.in);
         float amount;
         amount = newScan.nextFloat();
-        
         userSkatt = amount * (komunalskatt/100);
+        
+        // om du har inga pengar på ditt konto så varnar dig att du behöver sätta in pengar  eller ångrar dig 
         if (Meny.userBalance < userSkatt){
             System.out.println("Du har inget tillräckligt pengar, först sätta in pengar ");
             Meny.SkatteMainMenu();
         } else{
+            // skatten drar pengar från ditt konto
             Meny.userBalance -= userSkatt;
             System.out.println("Skatten "+ userSkatt + " är betald nu och du har " + Meny.userBalance + " " + Meny.userInputCurrency);
         }
